1b00adfa6c3f8d12bbecd2039f9064d8fd70a262
GibboK <gibbok.coding@gmail.com>
2020-03-28 22:48:13 +0100

    Print hello world

---
+++ src/ColorPickerPalette.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import ColorPickerPalette from './ColorPickerPalette';
+
+storiesOf('Components/Divider', module).add('Base', () => <ColorPickerPalette />);
