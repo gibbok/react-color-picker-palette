d956e58818523a978bc84ec5528fed5c94850a01
GibboK <gibbok.coding@gmail.com>
2020-03-28 23:26:43 +0100

    render gradient

--- src/ColorPickerPalette.tsx
+++ src/ColorPickerPalette.tsx
@@ -1,5 +1,42 @@
+/*! ColorPickerPalette | The MIT License (MIT) | Copyright (c) 2016 GibboK */
+
 import * as React from 'react';
 
-const ColorPickerPalette = () => <div>Hello</div>;
+const ColorPickerPalette = () => {
+  const canvasRef = React.createRef<HTMLCanvasElement>();
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        let gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+        gradient.addColorStop(0, 'rgb(255, 0, 0)');
+        gradient.addColorStop(0.15, 'rgb(255, 0, 255)');
+        gradient.addColorStop(0.33, 'rgb(0, 0, 255)');
+        gradient.addColorStop(0.49, 'rgb(0, 255, 255)');
+        gradient.addColorStop(0.67, 'rgb(0, 255, 0)');
+        gradient.addColorStop(0.84, 'rgb(255, 255, 0)');
+        gradient.addColorStop(1, 'rgb(255, 0, 0)');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, ctx.canvas.width, canvas.height);
+        gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+        gradient.addColorStop(0, 'rgba(255, 255, 255, 1)');
+        gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0)');
+        gradient.addColorStop(0.5, 'rgba(0, 0, 0, 0)');
+        gradient.addColorStop(1, 'rgba(0, 0, 0, 1)');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      }
+    }
+  });
+
+  return (
+    <div>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
+};
 
 export default ColorPickerPalette;
