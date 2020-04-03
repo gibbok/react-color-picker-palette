655120e5e8808104f036c4291035729f3eacc9ec
GibboK <gibbok.coding@gmail.com>
2020-04-03 23:07:34 +0200

    Add tests

--- cypress/integration/ColorPickerPalette.spec.ts
+++ cypress/integration/ColorPickerPalette.spec.ts
@@ -6,22 +6,29 @@ const RESULT_RGB = '[data-cy=result-rgb]';
 const PICKED_COLOR = '[data-cy=picked-color]';
 const PICKED_PREV_COLOR = '[data-cy=picked-prevColor]';
 
+const COLOR_1 = '#00fdec';
+const COLOR_2 = 'rgb(0, 253, 236)';
+const COLOR_3 = 'rgba(0, 0, 0, 0)';
+
 describe('ColorPickerPalette', () => {
   beforeEach(() => {
     cy.visit('http://localhost:6006/iframe.html?id=colorpickerpalette--light', { timeout: 10000 });
   });
 
-  describe('pickup color from palette', () => {
-    it('should display hex and rgb colors in results', () => {
-      cy.get(CANVAS).click();
-      cy.get(RESULT_HEX).should('contain', '#00fdec');
-      cy.get(RESULT_RGB).should('contain', 'rgb(0, 253, 236)');
-    });
+  it('should display results as transparent', () => {
+    cy.get(PICKED_COLOR).should('have.css', 'background-color', COLOR_3);
+    cy.get(PICKED_PREV_COLOR).should('have.css', 'background-color', COLOR_3);
+  });
 
-    it('should display picked with previouse and current colors', () => {
-      cy.get(CANVAS).click();
-      cy.get(PICKED_COLOR).should('have.css', 'background-color', 'rgb(0, 253, 236)');
-      cy.get(PICKED_PREV_COLOR).should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
-    });
+  it('should display hex and rgb colors in results', () => {
+    cy.get(CANVAS).click();
+    cy.get(RESULT_HEX).should('contain', COLOR_1);
+    cy.get(RESULT_RGB).should('contain', COLOR_2);
+  });
+
+  it('should display picked with previouse and current colors', () => {
+    cy.get(CANVAS).click();
+    cy.get(PICKED_COLOR).should('have.css', 'background-color', COLOR_2);
+    cy.get(PICKED_PREV_COLOR).should('have.css', 'background-color', COLOR_3);
   });
 });
