70d7a1940b0dc23ef96b488e5ba4bf32f237809f
GibboK <gibbok.coding@gmail.com>
2020-04-03 22:28:50 +0200

    Detect change in color in results

--- cypress/integration/ColorPickerPalette.spec.ts
+++ cypress/integration/ColorPickerPalette.spec.ts
@@ -11,11 +11,15 @@ describe('ColorPickerPalette', () => {
     cy.visit('http://localhost:6006/iframe.html?id=colorpickerpalette--light', { timeout: 10000 });
   });
 
-  describe('pickup color on palette', () => {
-    it('should display in results hex and rgb colors', () => {
+  describe('pickup color from palette', () => {
+    it('should display hex and rgb colors in results', () => {
       cy.get(CANVAS).click();
       cy.get(RESULT_HEX).should('contain', '#00fdec');
       cy.get(RESULT_RGB).should('contain', 'rgb(0, 253, 236)');
+    });
+
+    it('should display result with selected color and previouse color', () => {
+      cy.get(CANVAS).click();
       cy.get(PICKED_COLOR).should('have.css', 'background-color', 'rgb(0, 253, 236)');
       cy.get(PICKED_PREV_COLOR).should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
     });
